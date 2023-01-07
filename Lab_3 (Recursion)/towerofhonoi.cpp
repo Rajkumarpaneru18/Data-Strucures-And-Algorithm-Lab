02a1593e06c7147c06ec4c3feec188a693963c0c
Raj Kumar Paneru <rajkumarpaneru111@gmail.com>
2023-01-07 11:40:10 +0545

    slightly change in code

--- Lab_3 (Recursion)/towerofhonoi.cpp
+++ Lab_3 (Recursion)/towerofhonoi.cpp
@@ -1,20 +1,32 @@
 
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-void towerOfHanoi(int n, char source, char destination, char auxilary){ 
-    if(n==1){//base case
-        cout<<"move disk 1 from "<<source<<" to "<<destination<<endl;
-        return;
-    }
-    //recursion
-    towerOfHanoi(n-1,source,auxilary,destination);
-    cout<<"move disk "<< n<<" from "<<source <<" to "<<destination<<endl;
-    towerOfHanoi(n-1,auxilary , destination, source);
-
+void towerOfHanoi(int n, char source, char destination, char auxilary)
+{
+  if (n == 1)
+  { // base case
+    cout << "move disk 1 from " << source << " to " << destination << endl;
+    return;
+  }
+  // recursion
+  towerOfHanoi(n - 1, source, auxilary, destination);
+  cout << "move disk " << n << " from " << source << " to " << destination << endl;
+  towerOfHanoi(n - 1, auxilary, destination, source);
 }
-int main(){ 
-  
-  towerOfHanoi(4, 'S','D','A');
-    
+int main()
+{
+  int numberOfDisc;
+  cout << "Enter the number of disc: " << endl;
+  cin >> numberOfDisc;
+  char source, destination, auxilary;
+  cout << "Enter the source of a disc: " << endl;
+  cin >> source;
+  cout << "Enter the destination for disc where you want to store it: " << endl;
+  cin >> destination;
+  cout << "Enter an auxilary pole: " << endl;
+  cin >> auxilary;
+
+  towerOfHanoi(numberOfDisc, source, destination, auxilary);
+  return 0;
 }
